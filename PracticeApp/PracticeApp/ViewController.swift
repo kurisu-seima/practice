420d53582578b21bd8eae907f517a4ac18c7bbce
kurisu-seima <kurisuseima@gmail.com>
2021-08-22 03:27:45 +0900

    初期コミット

---
+++ PracticeApp/PracticeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeApp
+//
+//  Created by 栗須星舞 on 2021/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
